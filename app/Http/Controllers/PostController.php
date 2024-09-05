abf6e4c27905eba2176a6e838ce60a9227f83adc
tsuka716 <sakanahaoisiiyo123@gmail.com>
2024-09-05 22:45:53 +0900

    エラー表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,18 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
-      //getPaginateByLimit()はPost.phpで定義したメソッドです。
+      return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+
     public function show(Post $post)
     {
       return view('posts.show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+      return view('posts.create');
+    }
+
+    public function store(Post $post, PostRequest $request)
+    {
+      $input = $request['post'];
+      $post->fill($input)->save();
+      return redirect('/posts/' . $post->id);
     }
 }
 ?>
