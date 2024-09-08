f48b9f0a3e1a42285739ce717f85fe4bc8635da9
tsuka716 <sakanahaoisiiyo123@gmail.com>
2024-09-08 22:59:29 +0900

    論理削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,11 +35,17 @@ class PostController extends Controller
     }
 
     public function update(PostRequest $request, Post $post)
-{
+    {
     $input_post = $request['post'];
     $post->fill($input_post)->save();
 
     return redirect('/posts/' . $post->id);
-}
+    }
+
+    public function delete(Post $post)
+    {
+      $post->delete();
+      return redirect('/');
+    }
 }
 ?>
